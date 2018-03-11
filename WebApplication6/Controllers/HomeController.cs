1026e2063765834c435dd9a5a5aad9c4ddf5d45f
NetajiBandi <netaji@outlook.com>
2018-03-11 18:57:54 +0530

    tes checkin from vs

--- WebApplication6/Controllers/HomeController.cs
+++ WebApplication6/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace WebApplication6.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Test Your application description page.";
 
             return View();
         }
